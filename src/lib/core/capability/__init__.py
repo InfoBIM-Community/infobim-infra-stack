eccc42c068b9f72382b5234f4e3044ae8597ba58
Elias Magalhães <elias@brasidata.com.br>
2026-02-12 06:23:17 -0500

    Lib added

---
+++ src/lib/core/capability/__init__.py
@@ -0,0 +1,6 @@
+
+from .metadata import CapabilityMetadata
+from .base import Capability
+from .executor import CapabilityExecutor
+
+__all__ = ["Capability", "CapabilityMetadata", "CapabilityExecutor"]
